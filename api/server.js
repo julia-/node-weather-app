c7c831a07d3dc2c4b80221d1786614351e566250
julia- <julia.ryanx@gmail.com>
2017-12-05 12:58:11 +1100

    improve error handling

--- api/server.js
+++ api/server.js
@@ -10,5 +10,9 @@ server.use(bodyParser.json()) // allows JSON uploads(POST/PUT/PATCH)
 
 // Start server
 server.listen(7000, (error) => {
-  console.log('Server started at http://localhost:7000');
+  if (error) {
+    console.error('Error starting', error);
+  } else {
+    console.log('Server started at http://localhost:7000');
+  }
 })

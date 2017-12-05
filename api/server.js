349ea450ebaafae91c4a8c6c77c4b2cc3c6ea64c
julia- <julia.ryanx@gmail.com>
2017-12-05 12:40:56 +1100

    add body-parser

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const server = express()
 
+// Plugins
+server.use(bodyParser.json()) // allows JSON uploads(POST/PUT/PATCH)
+
+// Routes
+
+// Start server
 server.listen(7000, (error) => {
   console.log('Server started at http://localhost:7000');
 })
